c562b27fe27592e6ff89b730b8851b978e79df11
solrizal <rizal.solichudin@gmail.com>
2017-04-20 14:02:41 +0700

    latest change

--- js/controller.js
+++ js/controller.js
@@ -2,9 +2,31 @@ var test = function (a) {
 	console.log(a);
 }
 
-ajs.controller('HomeCtrl', ['$scope', 'video', function($scope, video) {
+ajs.controller('HomeCtrl', ['$scope', 'video', '$interval', '$rootScope', function($scope, video, $interval, $rootScope) {
 	$scope.$parent.select = 0;
 	video.addSource('mp4', 'content/final.mp4');
+	$scope.counter = 1;
+	$scope.addImg = function (n) {
+		$scope.showImg($scope.counter += n);
+	};
+	$scope.showImg = function (n) {
+		var slides = document.getElementsByClassName("slideImg");
+		if (n > slides.length) {$scope.counter = 1} 
+		if (n < 1) {$scope.counter = slides.length}
+		for (i = 0; i < slides.length; i++) {
+			slides[i].style.display = "none";
+		}
+		slides[$scope.counter-1].style.display = "block";
+	};
+	$scope.showImg($scope.counter);
+	$scope.reload = $interval(function () {
+			$scope.addImg(1);
+		}, 3000);
+	var dereg = $rootScope.$on('$locationChangeSuccess', function() {
+		$interval.cancel($scope.reload);
+		console.log('pindah');
+		dereg();
+	});
 }])
 
 ajs.controller('ProfileCtrl', function($scope) {
